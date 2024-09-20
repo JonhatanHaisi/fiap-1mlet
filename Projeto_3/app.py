97c95756a4d588b52fcab335e8a834be3d6e60d1
Jonhatan Haisis <jonathan_haisi@hotmail.com>
2024-09-19 21:38:03 -0300

    Criação dos modelos de ML para previsão de dados e pagina para previsão de casos/mortes po covid 19

--- Projeto_3/app.py
+++ Projeto_3/app.py
@@ -1,6 +1,8 @@
 from st_aggrid import AgGrid
 
 import streamlit as st
+import pandas as pd
+import joblib
 
 import utils.covid_global_utils as cgu
 
@@ -14,12 +16,12 @@ with covid_global_tab:
 
     covid_global = cgu.carrega_e_formata_dados_covid_global()
 
-    '## Analise acuulativa de casos de COVID 19'
+    '## Analise acumulativa de casos de COVID 19'
     st.plotly_chart(cgu.criar_grafico_evolucao(covid_global, 'Casos Acumulados', 'Novos casos de COVID-19 no mundo'))
     st.plotly_chart(cgu.criar_grafico_geo(covid_global, 'Casos Acumulados', 'Casos de COVID-19 no mundo'))
     st.plotly_chart(cgu.criar_grafico_acumulado(covid_global, 'Evolução de casos de COVID-19 no mundo', 'Casos Acumulados', 'Novos Casos'))
 
-    '## Analise acuulativa de mortes por COVID 19'
+    '## Analise acumulativa de mortes por COVID 19'
     st.plotly_chart(cgu.criar_grafico_evolucao(covid_global, 'Mortes Acumuladas', 'Novas mortes por COVID-19 no mundo'))
     st.plotly_chart(cgu.criar_grafico_geo(covid_global, 'Mortes Acumuladas', 'Mortes por COVID-19 no mundo'))
     st.plotly_chart(cgu.criar_grafico_acumulado(covid_global, 'Evolução de mortes por COVID-19 no mundo', 'Mortes Acumuladas', 'Novas Mortes'))
@@ -55,7 +57,34 @@ with vacinacao_tab:
 with previsoes:
     '# Previsões'
 
-    'Em breve...'
+    # FORMULARIO DE DADOS PARA ALIMETAÇÃO DO MODELO
+    '## Informe os dados para previsão'
+
+    regiao = st.selectbox('Região OMS', covid_global['Região OMS'].unique())
+    total_vacicacoes = st.number_input('Total de vacinações', min_value=0, value=0)
+    total_ultima_dose = st.number_input('Total de pessoas com ultima dose', min_value=0, value=0)
+    total_dose_reforco = st.number_input('Total de pessoas com dose de reforço', min_value=0, value=0)
+    prever = st.button('Prever')
+
+    if prever:
+        modelo_casos = joblib.load('models/model_covid_casos.pkl')
+        modelo_mortes = joblib.load('models/model_covid_mortes.pkl')
+
+        st.divider()
+
+        dados = [[regiao, total_vacicacoes, total_ultima_dose, total_dose_reforco]]
+        dados = pd.DataFrame(dados, columns=['Região OMS', 'Total de Vacinações', 'Pessoas com Última Dose', 'Pessoas com Dose de Reforço'])
+        previsao_casos = modelo_casos.predict(dados)[0]
+
+        dados = [[regiao, total_vacicacoes, total_ultima_dose, total_dose_reforco]]
+        dados = pd.DataFrame(dados, columns=['Região OMS', 'Total de Vacinações', 'Pessoas com Última Dose', 'Pessoas com Dose de Reforço'])
+        previsao_mortes = modelo_mortes.predict(dados)[0]
+
+        '## Previsão de casos de COVID 19'
+        f'Previsão de casos de COVID 19: {previsao_casos} (Erro Médio Absoluto Aproximado: 174898)'
+
+        '## Previsão de mortes por COVID 19'
+        f'Previsão de mortes por COVID 19: {previsao_mortes} (Erro Médio Absoluto Aproximado: 3593)'
 
 
      
